72d97377f63a17e757819c38439dcc10521c17fd
THEHACKILLER17 <shaikhzaid0903@gmail.com>
2026-06-04 15:41:41 +0530

    feat: add author footer with GitHub and LinkedIn links to layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,9 +5,14 @@ const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Sidebar />
-      <main className='main-content'>
-        {children}
-      </main>
+      <div className='main-wrapper'>
+        <main className='main-content'>
+          {children}
+        </main>
+        <footer className='app-footer'>
+          <p>Built by <a href='https://github.com/zaid-shaikh17' target='_blank' rel='noreferrer'>Zaid Shaikh</a> · MERN Stack Developer · <a href='https://www.linkedin.com/in/shaikh-zaid-m8329981925/' target='_blank' rel='noreferrer'>LinkedIn</a> · <a href='https://github.com/zaid-shaikh17/Billr-Frontend' target='_blank' rel='noreferrer'>GitHub</a></p>
+        </footer>
+      </div>
     </div>
   )
 }
